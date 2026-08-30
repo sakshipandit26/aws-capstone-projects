9cc1c91f9135b737bc752e01a6fc0a51d9b1b154
Sakshi Pandit <151164389+sakshipandit26@users.noreply.github.com>
2026-08-30 16:00:16 +0530

    Delete lambda_function.py

--- lambda_function.py
+++
@@ -1,7 +0,0 @@
-def lambda_handler(event, context):
-    print("===== CI/CD LAMBDA DEPLOYMENT SUCCESSFUL =====")
-
-    return {
-        "statusCode": 200,
-        "body": "Lambda deployed successfully through CI/CD!"
-    }
